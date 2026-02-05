80848e7bc74ee74cd46b141384f030ee4bf3e858
Zachary Waggoner <zwaggoner@comcast.net>
2026-02-04 20:44:08 -0500

    Minor fixes

--- examples/sm-example.rs
+++ examples/sm-example.rs
@@ -1,85 +1,85 @@
 extern crate rsm;
 
-use rsm::{Event, Action, StateMachine};
+use rsm::{Action, Event, StateMachine};
 
 #[derive(Clone, Copy)]
 enum UserEvents {
-    TestEvent
+    TestEvent,
 }
 
 struct ActorCtx {}
 
 struct Actor {
-    sm : StateMachine<ActorCtx, UserEvents>,
-    context : ActorCtx
+    sm: StateMachine<ActorCtx, UserEvents>,
+    context: ActorCtx,
 }
 
 impl Actor {
-    fn top_state(context: &mut ActorCtx, event: Event<UserEvents>) -> Action<ActorCtx, UserEvents> {
+    fn top_state(_context: &mut ActorCtx, event: Event<UserEvents>) -> Action<ActorCtx, UserEvents> {
         match event {
             Event::Entry => {
                 println!("Top State Entry");
                 Action::<ActorCtx, UserEvents>::Handled
-            },
+            }
             Event::Exit => {
                 println!("Top State Exit");
                 Action::<ActorCtx, UserEvents>::Handled
-            },
-            _ =>  {
-                Action::<ActorCtx, UserEvents>::Unhandled
             }
+            _ => Action::<ActorCtx, UserEvents>::Unhandled,
         }
     }
 
-    fn state1(context : &mut ActorCtx, event: Event<UserEvents>) -> Action::<ActorCtx, UserEvents> {
+    fn state1(_context: &mut ActorCtx, event: Event<UserEvents>) -> Action<ActorCtx, UserEvents> {
         match event {
             Event::GetParent => Action::<ActorCtx, UserEvents>::Parent(Self::top_state),
             Event::Entry => {
                 println!("State1 Entry");
                 Action::<ActorCtx, UserEvents>::Handled
-            },
+            }
             Event::Exit => {
                 println!("State1 Exit");
                 Action::<ActorCtx, UserEvents>::Handled
             }
-            Event::Other(user) => {
-                match user {
-                   UserEvents::TestEvent => Action::<ActorCtx, UserEvents>::Transition(Self::state2),
-                   _ => Action::<ActorCtx, UserEvents>::Unhandled,
-                }
+            Event::Other(user) => match user {
+                UserEvents::TestEvent => Action::<ActorCtx, UserEvents>::Transition(Self::state2),
+                _ => Action::<ActorCtx, UserEvents>::Unhandled,
             },
-            _ => Action::<ActorCtx, UserEvents>::Unhandled
+            _ => Action::<ActorCtx, UserEvents>::Unhandled,
         }
     }
 
-    fn state2(context : &mut ActorCtx, event: Event<UserEvents>) -> Action::<ActorCtx, UserEvents> {
+    fn state2(_context: &mut ActorCtx, event: Event<UserEvents>) -> Action<ActorCtx, UserEvents> {
         match event {
             Event::GetParent => Action::<ActorCtx, UserEvents>::Parent(Self::top_state),
             Event::Entry => {
                 println!("State2 Entry");
                 Action::<ActorCtx, UserEvents>::Handled
-            },
+            }
             Event::Exit => {
                 println!("State2 Exit");
                 Action::<ActorCtx, UserEvents>::Handled
             }
-            Event::Other(user) => {
-                match user {
-                   UserEvents::TestEvent => Action::<ActorCtx, UserEvents>::Transition(Self::state2),
-                   _ => Action::<ActorCtx, UserEvents>::Unhandled,
-                }
+            Event::Other(user) => match user {
+                UserEvents::TestEvent => Action::<ActorCtx, UserEvents>::Transition(Self::state2),
+                _ => Action::<ActorCtx, UserEvents>::Unhandled,
             },
-            _ => Action::<ActorCtx, UserEvents>::Unhandled
+            _ => Action::<ActorCtx, UserEvents>::Unhandled,
         }
     }
 }
 
 fn main() {
-    let mut actor = Actor { sm : StateMachine::default(), context : ActorCtx{} };
+    let mut actor = Actor {
+        sm: StateMachine::default(),
+        context: ActorCtx {},
+    };
 
     {
         actor.sm.initial(&mut actor.context, Actor::state1);
     }
 
-    actor.sm.dispatch(&mut actor.context, Event::<UserEvents>::Other(UserEvents::TestEvent));
+    actor.sm.dispatch(
+        &mut actor.context,
+        Event::<UserEvents>::Other(UserEvents::TestEvent),
+    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,39 @@
 #[derive(Clone, Copy)]
-pub enum Event<Et : Copy> {
+pub enum Event<Et: Copy> {
     GetParent,
     Initial,
     Entry,
     Exit,
-    Other(Et)
+    Other(Et),
 }
 
-pub enum Action<C, Et : Copy> {
+pub enum Action<C, Et: Copy> {
     Parent(State<C, Et>),
     Unhandled,
     Handled,
-    Transition(State<C, Et>)
+    Transition(State<C, Et>),
 }
 
-type State<C, Et : Copy> = fn(&mut C, Event<Et>) -> Action<C, Et>;
+type State<C, Et: Copy> = fn(&mut C, Event<Et>) -> Action<C, Et>;
 
-pub struct StateMachine<C, Et : Copy, const MAX_NEST_DEPTH : usize = 32> {
-    path : [Option<State<C, Et>>; MAX_NEST_DEPTH],
-    curr_depth : usize
+pub struct StateMachine<C, Et: Copy, const MAX_NEST_DEPTH: usize = 32> {
+    path: [Option<State<C, Et>>; MAX_NEST_DEPTH],
+    curr_depth: usize,
 }
 
-impl<C, Et : Copy, const MAX_NEST_DEPTH : usize> StateMachine<C, Et, MAX_NEST_DEPTH> {
+impl<C, Et: Copy, const MAX_NEST_DEPTH: usize> StateMachine<C, Et, MAX_NEST_DEPTH> {
     pub fn default() -> Self {
-            Self { path : [None; MAX_NEST_DEPTH], curr_depth: 0 }
+        Self {
+            path: [None; MAX_NEST_DEPTH],
+            curr_depth: 0,
+        }
     }
 
-    pub fn initial(&mut self, context : &mut C, state : State<C, Et>) {
+    pub fn initial(&mut self, context: &mut C, state: State<C, Et>) {
         self.enter_from(context, state, 0);
     }
 
-    fn enter_from(&mut self, context : &mut C, state : State<C, Et>, start : usize) {
+    fn enter_from(&mut self, context: &mut C, state: State<C, Et>, start: usize) {
         // Take initial transitions until we find the bottom-most initial state
         let mut initial_state = state;
 
@@ -39,18 +42,17 @@ impl<C, Et : Copy, const MAX_NEST_DEPTH : usize> StateMachine<C, Et, MAX_NEST_DE
         }
 
         // Traverse parents until we find a shared one, or we reach the top
-        let mut depth : usize = start;
+        let mut depth: usize = start;
         let mut curr_state = initial_state;
-        let mut reverse_path : [Option<State<C, Et>>; MAX_NEST_DEPTH] = [None; MAX_NEST_DEPTH];
+        let mut reverse_path: [Option<State<C, Et>>; MAX_NEST_DEPTH] = [None; MAX_NEST_DEPTH];
 
         reverse_path[depth] = Some(curr_state);
         depth += 1;
 
         while let Action::Parent(parent_state) = (curr_state)(context, Event::GetParent) {
-            if self.path.contains(&Some(parent_state)) {
+            if self.path[..depth].contains(&Some(parent_state)) {
                 break;
-            }
-            else {
+            } else {
                 reverse_path[depth] = Some(parent_state);
                 curr_state = parent_state;
                 depth += 1;
@@ -66,36 +68,29 @@ impl<C, Et : Copy, const MAX_NEST_DEPTH : usize> StateMachine<C, Et, MAX_NEST_DE
 
         while let Some(state) = self.path[depth] {
             (state)(context, Event::Entry);
-            depth += 1;                
+            depth += 1;
         }
-        
+
         self.curr_depth = depth - 1;
     }
 
-    fn exit_to(&mut self, context : &mut C, end : usize) {
+    fn exit_to(&mut self, context: &mut C, end: usize) {
         for depth in (end..=self.curr_depth).rev() {
             if let Some(state) = self.path[depth] {
                 (state)(context, Event::Exit);
 
                 if self.curr_depth > 0 {
                     self.curr_depth -= 1;
-                }
-                else {
+                } else {
                     break;
                 }
             }
         }
     }
 
-    pub fn dispatch(&mut self, context: &mut C, event : Event<Et>) {
+    pub fn dispatch(&mut self, context: &mut C, event: Event<Et>) {
         for depth in (0..=self.curr_depth).rev() {
-            let mut curr_path : Option<State<C, Et>> = None;
-
-            {
-                curr_path = self.path[depth]
-            }
-
-            if let Some(state) =  curr_path {
+            if let Some(state) = self.path[depth] {
                 match (state)(context, event) {
                     Action::<C, Et>::Handled => break,
                     Action::<C, Et>::Transition(new_state) => {
@@ -103,43 +98,43 @@ impl<C, Et : Copy, const MAX_NEST_DEPTH : usize> StateMachine<C, Et, MAX_NEST_DE
                             // Exit and re-enter same state;
                             self.exit_to(context, depth);
                             self.enter_from(context, new_state, depth);
-                        } 
-                        else {
+                        } else {
                             // Find shared parent (if any)
                             let mut curr_state = new_state;
-                            let mut parent_state : Option<State<C, Et>> = None;
+                            let mut parent_state: Option<State<C, Et>> = None;
                             let mut transition_depth = depth;
 
-                            while let Action::Parent(new_parent_state) = (curr_state)(context, Event::GetParent) {
-                                if let Some(shared_parent_depth) = self.path.iter().position(|&state| state == Some(new_parent_state)) {
+                            while let Action::Parent(new_parent_state) =
+                                (curr_state)(context, Event::GetParent)
+                            {
+                                if let Some(shared_parent_depth) = self.path[..self.curr_depth]
+                                    .iter()
+                                    .position(|&state| state == Some(new_parent_state))
+                                {
                                     transition_depth = shared_parent_depth + 1;
                                     parent_state = Some(new_parent_state);
                                     break;
-                                }
-                                else {
+                                } else {
                                     curr_state = new_parent_state;
                                 }
                             }
 
-
-                            if let Some(shared_parent) = parent_state {
-                                self.exit_to(context, transition_depth);        
+                            if let Some(_shared_parent) = parent_state {
+                                self.exit_to(context, transition_depth);
                                 self.enter_from(context, new_state, transition_depth);
-                            }
-                            else {
+                            } else {
                                 // We reached the top with no shared parent, top state changed
                                 self.exit_to(context, 0);
                                 self.enter_from(context, new_state, 0);
                             }
                         }
                         break;
-                    },
-                    _ => continue
+                    }
+                    _ => continue,
                 }
             }
         }
     }
 
-    pub fn run(&mut self, context : &mut C) {
-    }
+    pub fn run(&mut self, context: &mut C) {}
 }
