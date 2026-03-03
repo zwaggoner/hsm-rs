2481cb8d30edd7bb2fc3636c4b5fd4d874d15de4
Zachary Waggoner <zwaggoner@comcast.net>
2026-03-02 19:08:28 -0500

    Cargo clippy

--- src/fixed_vec.rs
+++ src/fixed_vec.rs
@@ -21,7 +21,7 @@ impl<T: Copy, const MAX_DEPTH: usize> FixedVec<T, MAX_DEPTH> {
             return Ok(());
         }
 
-        return Err(());
+        Err(())
     }
 
     pub(crate) fn pop(&mut self) -> Option<T> {
